489e36e93efa84cf89110f55d40bf6aff20331d9
Amit Zur <amit.zur@applitools.com>
2020-11-10 23:04:06 +0200

    measurements

--- applitools.config.js
+++ applitools.config.js
@@ -1,20 +1,15 @@
 const isDocker = require("is-docker");
+const BROWSERS = Number(process.env.MEASURE_BROWSERS)
 module.exports = {
-  concurrency: 100,
+  concurrency: 5,
   showLogs: !!process.env.APPLITOOLS_SHOW_LOGS,
   appName: "Storybook testkit",
   batchName: "Storybook testkit",
-  browser: [
-    // { width: 800, height: 600, name: "chrome" },
-    // { width: 800, height: 600, name: "firefox" },
-    // {width: 800, height: 600, name: 'edge'},
-    // {width: 800, height: 600, name: 'ie11'},
-    { width: 1200, height: 800, name: "chrome" }
-    // { width: 1200, height: 800, name: "firefox" }
-    // {width: 1200, height: 800, name: 'edge'},
-    // {width: 1200, height: 800, name: 'ie11'},
-    // { deviceName: "iPhone X" }
-  ],
+  browser: new Array(BROWSERS).fill().map((_, i) => ({
+    name: "chrome",
+    width: 600 + 25 * i,
+    height: 600 + 25 * i
+  })),
   runInDocker: isDocker(),
   puppeteerOptions: {
     // headless: false,
