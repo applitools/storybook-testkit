66f6de0dd976992308b6b76a4b0bc04636325097
Amit Zur <amit.zur@applitools.com>
2020-11-17 10:11:00 +0200

    demonstrate local waitBeforeScreenshot overrides global config

--- applitools.config.js
+++ applitools.config.js
@@ -4,18 +4,10 @@ module.exports = {
   showLogs: !!process.env.APPLITOOLS_SHOW_LOGS,
   appName: "Storybook testkit",
   batchName: "Storybook testkit",
-  browser: [
-    // { width: 800, height: 600, name: "chrome" },
-    // { width: 800, height: 600, name: "firefox" },
-    // {width: 800, height: 600, name: 'edge'},
-    // {width: 800, height: 600, name: 'ie11'},
-    { width: 1200, height: 800, name: "chrome" }
-    // { width: 1200, height: 800, name: "firefox" }
-    // {width: 1200, height: 800, name: 'edge'},
-    // {width: 1200, height: 800, name: 'ie11'},
-    // { deviceName: "iPhone X" }
-  ],
+  browser: [{ width: 1200, height: 800, name: "chrome" }],
   runInDocker: isDocker(),
+  waitBeforeScreenshot: 2000,
+  include: ({ name }) => name === "appears after a delay",
   puppeteerOptions: {
     // headless: false,
     // devtools: true,
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -8,3 +8,31 @@ const COUNT = Number(process.env.STORYBOOK_COUNT) || 1200;
 for (let i = 0, ii = COUNT; i < ii; i++) {
   storiesOf("Perf", module).add("Test page " + i, () => <Test index={i} />);
 }
+
+storiesOf("Text", module).add(
+  "appears after a delay",
+  () => (
+    <WithScript
+      script={() => {
+        window.setTimeout(() => {
+          document.getElementById("delay").innerText =
+            "Now Ready - snapshot me!";
+          document.getElementById("delay").className = "ready";
+        }, 4000);
+      }}
+    >
+      <div id="delay">Not Ready - do not take snapshot!</div>
+    </WithScript>
+  ),
+  { eyes: { waitBeforeScreenshot: ".ready" } }
+);
+
+class WithScript extends React.Component {
+  componentDidMount() {
+    this.props.script();
+  }
+
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
